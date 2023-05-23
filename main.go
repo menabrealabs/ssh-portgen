45b43262a4dbcd3112b70eb40d533af006b4e48a
Nyk Cowham <nyk@cowham.net>
2023-05-23 09:17:16 +0700

    Use hostname syscall rather than environment variable.

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -13,30 +15,47 @@ const (
 )
 
 func main() {
-	hostname := getHostname()
-	digest := getDigest(hostname)
+	hostname, err := getHostname()
+	if err != nil {
+		log.Fatalf("Cannot read hostname: \n%s\n", usage())
+	}
+
+	digest, err := getDigest(hostname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	port := getPort(digest)
+
+	// TODO: Check whether the port number is bound on current host
 
 	// Print the output
 	fmt.Printf("Hostname: %s\n", hostname)
-	fmt.Printf("SHA256 Digest: %x\n", digest)
+	fmt.Printf("SHA2 Digest: %x\n", digest)
 	fmt.Printf("SSH port number: %d\n", port)
 }
 
+func usage() string {
+	return "Usage: ssh-portgen [hostname]"
+}
+
 // Get the hostname from the command line or from the environment.
-func getHostname() string {
+func getHostname() (string, error) {
 	if len(os.Args) > 1 {
-		return os.Args[1]
+		return os.Args[1], nil
 	} else {
-		return os.Getenv("HOSTNAME")
+		return os.Hostname()
 	}
 }
 
 // Get a Sha256 digest of the hostname.
-func getDigest(hostname string) []byte {
+func getDigest(hostname string) ([]byte, error) {
 	b := sha256.New()
-	b.Write([]byte(hostname))
-	return b.Sum(nil)
+	_, err := b.Write([]byte(hostname))
+	if err != nil {
+		return nil, errors.New("Cannot generate SHA-256 digest")
+	}
+	return b.Sum(nil), nil
 }
 
 // Convert digest bytes into unsigned int and remove last digit.
