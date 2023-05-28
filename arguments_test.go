7966c6339e8d895a43e39d4d86a337aefb990a25
Nyk Cowham <nyk@cowham.net>
2023-05-28 13:23:52 +0700

    Added arguments tests

--- arguments_test.go
+++ arguments_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestArgs_should_set_var_to_default_value(t *testing.T) {
-	var indices digestIndices
+	var indices digestIndex
 	err := indices.Set("2/20")
 	if err != nil {
 		t.Error(err)
@@ -13,7 +13,7 @@ func TestArgs_should_set_var_to_default_value(t *testing.T) {
 }
 
 func TestArgs_one_value_should_fail(t *testing.T) {
-	var indices digestIndices
+	var indices digestIndex
 	err := indices.Set("2")
 	if err == nil {
 		t.Errorf("Expected error on single value but got %s", indices.String())
@@ -21,17 +21,17 @@ func TestArgs_one_value_should_fail(t *testing.T) {
 }
 
 func TestArgs_above_upper_bound_value_should_fail(t *testing.T) {
-	var indices digestIndices
-	err := indices.Set("2/33")
+	var index digestIndex
+	err := index.Set("2/33")
 	if err == nil {
-		t.Errorf("Expected error when asking for index out of upper bounds but got %s", indices.String())
+		t.Errorf("Expected error when asking for index out of upper bounds but got %s", index.String())
 	}
 }
 
 func TestArgs_above_lower_bound_value_should_fail(t *testing.T) {
-	var indices digestIndices
-	err := indices.Set("-1/33")
+	var index digestIndex
+	err := index.Set("-1/33")
 	if err == nil {
-		t.Errorf("Expected error when asking for index out of lower bounds but got %s", indices.String())
+		t.Errorf("Expected error when asking for index out of lower bounds but got %s", index.String())
 	}
 }
