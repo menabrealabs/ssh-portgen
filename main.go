0b2919f29176e7fcf4c2fcd0445aa2c63de066e0
Nyk Cowham <nyk@cowham.net>
2023-05-30 18:58:10 +0700

    refactoring main.go

--- main.go
+++ main.go
@@ -7,18 +7,18 @@ import (
 )
 
 const (
-	byteSize       = 8
-	digestBitSize  = 256
-	digestByteSize = digestBitSize / byteSize
+	byteSize       = 0x0008                   // 8
+	digestBitSize  = 0x0100                   // 256
+	digestByteSize = digestBitSize / byteSize // 32
 )
 
-var indicesFlag = digestIndex{2, 20}
+var indexFlag = digestIndex{2, 20}
 var rawFlag bool
 var lowFlag bool
 
 func init() {
-	flag.Var(&indicesFlag, "indices", "index of two bytes from the digest in the range 0..31, separated by a forward-slash: e.g. 2/20")
-	flag.Var(&indicesFlag, "i", "shorthand for -indices flag")
+	flag.Var(&indexFlag, "indices", "index of two bytes from the digest in the range 0..31, separated by a forward-slash: e.g. 2/20")
+	flag.Var(&indexFlag, "i", "shorthand for -indices flag")
 	flag.BoolVar(&rawFlag, "raw", false, "raw output: set to true to print only the raw generated port number")
 	flag.BoolVar(&rawFlag, "r", false, "shorthand for -raw flag")
 	flag.BoolVar(&lowFlag, "low", false, "allow high (5 digit) port numbers")
@@ -34,7 +34,7 @@ func main() {
 	digest, err := getDigest(hostname)
 	logFatal(err)
 
-	port, err := getPort(digest, indicesFlag, lowFlag)
+	port, err := getPort(digest, indexFlag, lowFlag)
 	logFatal(err)
 
 	// Print the output
